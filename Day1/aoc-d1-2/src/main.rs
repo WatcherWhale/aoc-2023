ae110886aab0c586582bf885388cb28302ba61ee
Mathias Maes <watcherwhale@maes.family>
2023-12-07 19:08:18 +0100

    finish aoc-d1-2

--- Day1/aoc-d1-2/src/main.rs
+++ Day1/aoc-d1-2/src/main.rs
@@ -1,7 +1,10 @@
 mod input;
 
-use self::input::read_file;
 use std::env;
+use self::input::read_file;
+
+extern crate sorted_list;
+use sorted_list::SortedList;
 
 fn main() {
     let args : Vec<String> = env::args().collect();
@@ -17,23 +20,30 @@ fn main() {
 }
 
 fn extract_value(input: &str) -> u32 {
-    let digits : String = replace_word_digits(input).chars().filter(|c| c.is_digit(10)).collect();
+    let digits : String = find_digits(input);
 
     let first = digits.chars().nth(0).unwrap().to_string();
     let last = digits.chars().nth(digits.len() - 1).unwrap().to_string();
 
-
     (first + &last).parse::<u32>().unwrap()
 }
 
-fn replace_word_digits(input: &str) -> String {
-    const NUMBERS: &'static [&'static str] = &["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-    let mut replaced = String::from(input);
+fn find_digits(input: &str) -> String {
+    const NUMBERS: &'static [&'static str] = &["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+    let mut list : SortedList<i32, String> = SortedList::new();
 
     for (i, num_str) in (*NUMBERS).iter().enumerate() {
-        let num = (i + 1).to_string();
-        replaced = replaced.replace(&num_str.to_string(), &num);
+        let indices : Vec<_> = input.match_indices(num_str).map(|(i, _)|i).collect();
+        for index in indices {
+            list.insert(index as i32, ( (i % 9) + 1).to_string());
+        }
     }
 
-    replaced.to_string()
+    let mut result : String = String::new();
+    for (_, kv) in list.iter().enumerate() {
+        result.push_str(kv.1);
+    }
+
+    result
 }
